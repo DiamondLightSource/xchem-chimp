92aefe168548ba89de91a4102af21714882478e8
David Erb <david.erb@diamond.ac.uk>
2023-04-09 04:42:20 +0100

    fixes version imports

--- src/xchem_chimp/version.py
+++ src/xchem_chimp/version.py
@@ -1,5 +1,11 @@
 import logging
 
+import albumentations
+import imageio
+import skimage
+import torch
+import torchvision
+
 import xchem_chimp
 
 logger = logging.getLogger(__name__)
@@ -22,8 +28,11 @@ def meta(given_meta=None):
     """
     s = {}
     s["xchem_chimp"] = version()
-
-    s.update(dls_utilpack.version.meta())
+    s["imageio"] = imageio.__version__
+    s["skimage"] = skimage.__version__
+    s["torch"] = torch.__version__
+    s["torchvision"] = torchvision.__version__
+    s["albumentations"] = albumentations.__version__
 
     if given_meta is not None:
         given_meta.update(s)
